89970fa701a1ec3eabcc9c68ef0aa66b35d73ee9
topchaluntron <6710110084@gmail.com>
2025-02-20 22:36:27 +0700

    commit routes

--- routes.py
+++ routes.py
@@ -1,34 +1,33 @@
 from models import db
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for , flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
+from flask_login import login_user , logout_user , login_required , current_user
+from models import User , Book
 
 app_routes = Blueprint('main', __name__)
 
 @app_routes.route('/')
 def home():
-    return redirect(url_for("app_routes.index"))
-# @app_routes.route('/dashboard')
-# def dashboard():
-#     return render_template('dashboard.html')
+    return render_template('index.html')
 
+@app_routes.route('/about')
+def about():
+    return render_template('about.html')
 
-# @app_routes.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         email = request.form.get('email')
-#         password = request.form.get('password')
+@app_routes.route('/register' , methods = ['Get' , 'Post'])
+def register():
+    if request.method == 'Post':
+        username = request.form.get('username')
+        password =  request.form.get('password')
+        
+        if not username or not password:
+            flash('กรอกครบอ้ะยางง','danger ')
+            return redirect(url_for('main.register'))
+        
+        user = User.query.filter_by(username = username).first()
+        if user :
+            flash('ชื่อนี้ถูกใช้แล้ว', 'danger')
+            return redirect(url_for('main.register'))
 
-#         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        
 
-#         new_user = User(username=username, email=email, password=hashed_password)
-#         db.session.add(new_user)
-#         db.session.commit()
-
-#         return redirect(url_for('auth.login'))
-#     return render_template('register.html')
-
-# @app_routes.route('/login', methods=['GET', 'POST'])
-# def login():
-#     return render_template('login.html')
