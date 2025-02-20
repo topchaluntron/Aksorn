8f0fbb9e987973d22ac3d6010286ad2833af29b0
topchaluntron <6710110084@gmail.com>
2025-02-20 21:31:17 +0700

    models_I

--- models.py
+++ models.py
@@ -1,22 +1,26 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login  import UserMixin
+from datetime import datetime
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(150), unique=True, nullable=False)
-#     email = db.Column(db.String(150), unique=True, nullable=False)
-#     password = db.Column(db.String(150), nullable=False)
+class User(UserMixin , db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
-# class Book(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(255), nullable=False)
-#     total_pages = db.Column(db.Integer, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    book = db.relationship('Book',backref = 'reader' , lazy = True)
+    
 
-# class Review(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     rating = db.Column(db.Integer, nullable=False)
-#     comment = db.Column(db.Text, nullable=True)
-#     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    total_pages = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
